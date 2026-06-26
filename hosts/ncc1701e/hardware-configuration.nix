47e994b965441820da8f9f5d77af0d1426c2b01d
Brandon Pollack <brandonpollack23@gmail.com>
2026-06-26 14:45:17 +0900

    nvidia on desktop

--- hosts/ncc1701e/hardware-configuration.nix
+++ hosts/ncc1701e/hardware-configuration.nix
@@ -43,4 +43,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # nvidia gpu setup
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true;
 }
