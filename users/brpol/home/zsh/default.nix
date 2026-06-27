c5011fe34de94338a4a0f7a5d70c73bb7e7c6ff5
Brandon Pollack <brandonpollack23@gmail.com>
2026-06-28 08:27:38 +0900

    more fzf tab config

--- users/brpol/home/zsh/default.nix
+++ users/brpol/home/zsh/default.nix
@@ -90,6 +90,29 @@
         zstyle ':completion:*' use-cache on
         zstyle ':completion:*' cache-path ~/.zsh/cache
 
+        # ── fzf-tab ───────────────────────────────────────────────────────────
+        # disable sort when completing `git checkout`
+        zstyle ':completion:*:git-checkout:*' sort false
+        # set descriptions format to enable group support
+        # NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+        zstyle ':completion:*:descriptions' format '[%d]'
+        # set list-colors to enable filename colorizing
+        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+        # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+        zstyle ':completion:*' menu no
+        # preview directory's content with eza when completing cd
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+        # custom fzf flags; fzf-tab does not follow FZF_DEFAULT_OPTS by default
+        # header-first pins the hint above the prompt; ^space is fzf's default toggle (multi-select)
+        zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept --header='^space:toggle <>:group ^f/^b:page /:continuous-sel' --header-first
+        # make fzf-tab follow FZF_DEFAULT_OPTS
+        # NOTE: may cause unexpected behavior since some flags break this plugin (see Aloxaf/fzf-tab#455)
+        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+        # switch group using `<` and `>`
+        zstyle ':fzf-tab:*' switch-group '<' '>'
+        # use tmux popup for fzf
+        zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
         # ── Vi mode ───────────────────────────────────────────────────────────
         bindkey -v
         bindkey -M viins 'jk' vi-cmd-mode
