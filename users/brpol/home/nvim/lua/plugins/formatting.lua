37eebd40e199451635e9e036c083a9a79ff3b04f
Brandon Pollack <brandonpollack23@gmail.com>
2026-07-22 11:50:14 +0900

    max width md

--- users/brpol/home/nvim/lua/plugins/formatting.lua
+++ users/brpol/home/nvim/lua/plugins/formatting.lua
@@ -2,6 +2,17 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        prettier = {
+          prepend_args = function(_, ctx)
+            local filetype = vim.bo[ctx.buf].filetype
+            if filetype == "markdown" or filetype == "markdown.mdx" then
+              return { "--print-width", "100", "--prose-wrap", "always" }
+            end
+            return {}
+          end,
+        },
+      },
       formatters_by_ft = {
         htmldjango = { "djlint" },
         -- keep html too if not already set by LazyVim extra
